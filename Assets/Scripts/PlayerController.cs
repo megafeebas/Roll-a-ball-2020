d01ff3506ad67f295db385e56d59bc1ba5665ea4
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-01-28 23:57:29 -0500

    Finished Text Popups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
     private int count;
+    public Text countText;
+    public Text winText;
 
    void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -27,7 +32,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up")) ;
         {
             count++;
+            SetCountText();
             other.gameObject.SetActive(false);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 9)
+        {
+            winText.text = "You Win!";
         }
     }
 }
