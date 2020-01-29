fe63aaeb3c751b0dfcf6f9fb38c42e114c071eb6
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-01-28 23:16:41 -0500

    Fixed Camera Issues

--- Assets/Scripts/CameraController2.cs
+++ Assets/Scripts/CameraController2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController2 : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
