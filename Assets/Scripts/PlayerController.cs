e0b8bc05052c8f3db09a5f7cd00b338b0d4bae28
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-01-28 23:01:17 -0500

    Started Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Rigidbody.AddForce();
+    }
+}
